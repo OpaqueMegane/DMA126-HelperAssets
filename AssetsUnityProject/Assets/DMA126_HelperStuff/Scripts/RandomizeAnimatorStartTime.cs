d20031eb7510671f2caaf43df48d2e821765db29
8_5X11\alexr <alexrickett@ucla.edu>
2025-11-04 10:48:14 -0800

    also randomize speed

--- AssetsUnityProject/Assets/DMA126_HelperStuff/Scripts/RandomizeAnimatorStartTime.cs
+++ AssetsUnityProject/Assets/DMA126_HelperStuff/Scripts/RandomizeAnimatorStartTime.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class RandomizeAnimatorStartTime : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float minRandomizedSpeed = .95f;
+    public float maxRandomizedSpeed = 1.05f;
     void Start()
     {
         var animator = this.GetComponent<Animator>();
+        animator.speed = Random.Range(minRandomizedSpeed, maxRandomizedSpeed);
         animator.Play(animator.GetCurrentAnimatorStateInfo(0).shortNameHash, 0, Random.Range(0f, 1f));   
     }
 
