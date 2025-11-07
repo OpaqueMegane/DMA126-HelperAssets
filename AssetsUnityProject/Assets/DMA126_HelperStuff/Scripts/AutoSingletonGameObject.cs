f12ca24b735811426528be0a8b4ad86c3ba73df4
8_5X11\alexr <alexrickett@ucla.edu>
2025-11-07 14:03:04 -0800

    improve singleton, and fix bug when using FindFirstObjectByType in Awake/Start

--- AssetsUnityProject/Assets/DMA126_HelperStuff/Scripts/AutoSingletonGameObject.cs
+++ AssetsUnityProject/Assets/DMA126_HelperStuff/Scripts/AutoSingletonGameObject.cs
@@ -7,15 +7,22 @@ public class AutoSingletonGameObject : MonoBehaviour
 {
     static Dictionary<string, AutoSingletonGameObject> _instances = new();
 
+    public string key = "Dialogue System_126"; //won't be affected by duplications, like the raw 'this.name' will
+
     void Awake()
     {
-        if (_instances.ContainsKey(gameObject.name))
+        if (_instances.ContainsKey(key))
         {
+            this.gameObject.SetActive(false); //<-- IMPORTANT!!!!
+            //The above deactivate prevents GameObject.FindFirtObjectByType from
+            //grabbing a soon-to-be, but not-immediately destroyed redundant object
+            //Could use DestroyImmediate as well, but discouraged by Unity
+            
             Destroy(this.gameObject);
             return;
         }
 
         DontDestroyOnLoad(this.gameObject.gameObject);
-        _instances[this.gameObject.name] = this;
+        _instances[key] = this;
     }
 }
