6addfb441af174fe752c071b5db8f65931fa2c72
8_5X11\alexr <alexrickett@ucla.edu>
2025-11-07 10:36:30 -0800

    make character controller teleportable, and respect starting rotation

--- AssetsUnityProject/Assets/DMA126_HelperStuff/3rdParty/Mini First Person Controller/Scripts/FirstPersonLook.cs
+++ AssetsUnityProject/Assets/DMA126_HelperStuff/3rdParty/Mini First Person Controller/Scripts/FirstPersonLook.cs
@@ -7,7 +7,7 @@ public class FirstPersonLook : MonoBehaviour
     public float sensitivity = 2;
     public float smoothing = 1.5f;
 
-    Vector2 velocity;
+
     Vector2 frameVelocity;
 
 
@@ -25,15 +25,19 @@ public class FirstPersonLook : MonoBehaviour
 
     void Update()
     {
+        Vector2 velocity = Vector2.zero;
         // Get smooth velocity.
         Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
         frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
-        velocity += frameVelocity;
-        velocity.y = Mathf.Clamp(velocity.y, -90, 90);
 
-        // Rotate camera up-down and controller left-right from velocity.
-        transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
-        character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+
+        var finVert = transform.localEulerAngles + new Vector3(-frameVelocity.y, 0, 0);
+        finVert.x = Mathf.DeltaAngle(0, finVert.x);
+        finVert.x = Mathf.Clamp(finVert.x, -90, 90);
+        transform.localEulerAngles = finVert;
+
+        var finHoriz = character.localEulerAngles + new Vector3(0, frameVelocity.x,  0);
+        character.localEulerAngles = finHoriz;
     }
 }
